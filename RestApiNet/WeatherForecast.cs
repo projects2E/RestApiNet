7790b7ce616f71d5c7a9c2533577afaf00d2e982
xroot <xroot@XROOT>
2024-02-21 15:01:54 -0500

    add new field in response

--- RestApiNet/WeatherForecast.cs
+++ RestApiNet/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace RestApiNet
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public string Title { get; set; }
     }
 }
